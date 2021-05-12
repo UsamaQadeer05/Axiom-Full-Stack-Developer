00add7d34337e7e4bab75152ca1ab7ab6f6991c2
Usama Qadeer <66266873+UsamaQadeer05@users.noreply.github.com>
2021-05-12 03:58:13 -0700

    Project 1 add showError function
    Added showError function on Project 1 script.js

--- Project1-Form-Validation/script.js
+++ Project1-Form-Validation/script.js
@@ -5,12 +5,26 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+// Function to Update class and error message
+function showError(input, message) {
+  // Get the parent element of the input field(.form-control)
+  const formControl = input.parentElement;
+  // Override the class - add error
+  formControl.className = "form-control error";
+  // Get the small element for the error message
+  const small = formControl.querySelector("small");
+  // Override the text for small element using the input message
+  small.innerText = message;
+}
+
+// Event Listeners
 // Create Event Listener for Submit Button
 form.addEventListener("submit", function (e) {
   // Stop Page Reloading on Submit Btn
   e.preventDefault();
-  console.log(username.value);
-  console.log(email.value);
-  console.log(password.value);
-  console.log(password2.value);
+
+  // Check if Username input is empty
+  if (username.value === "") {
+    showError(username, "Username is Required");
+  }
 });
