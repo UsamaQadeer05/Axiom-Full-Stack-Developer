5aa54db71673865827d108869b0790d9bd4ef31a
Usama Qadeer <66266873+UsamaQadeer05@users.noreply.github.com>
2021-05-12 05:24:59 -0700

    Project 1 JS
    Added functionality to check email validation, check length validation, and password match validation

--- Project1-Form-Validation/script.js
+++ Project1-Form-Validation/script.js
@@ -26,10 +26,14 @@ function showSuccess(input) {
 }
 
 // Function to Check Email Validate
-function isValidEmail(email) {
+function checkEmail(input) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `${getFieldId(input)} is Invalid`);
+  }
 }
 
 // Function to Check if required field have data
@@ -43,8 +47,34 @@ function checkRequired(inputArray) {
 }
 
 // Function to get the id of the input field with proper case
-function getFieldId(element){
-    return element.id.charAt(0).toUpperCase() + element.id.slice(1);
+function getFieldId(element) {
+  return element.id.charAt(0).toUpperCase() + element.id.slice(1);
+}
+
+// Function to Check Length of input field
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldId(input)} need to be atleast ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldId(input)} need to be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Function to Check if password and confirm password doesn't match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, `Password & ${getFieldId(input2)} doesn't match`);
+  } else {
+    showSuccess(input2);
+  }
 }
 
 // Event Listeners
@@ -54,4 +84,8 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 10);
+  checkLength(password, 6, 30);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
 });
