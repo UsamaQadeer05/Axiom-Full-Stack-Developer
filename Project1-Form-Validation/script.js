98658d3f42743d549aaf53f272c8f00e8ceb9c6b
Usama Qadeer <66266873+UsamaQadeer05@users.noreply.github.com>
2021-05-12 03:31:37 -0700

    Project 1 Verifying Submit Btn
    Verifying Project 1 submit button on script.js is working fine

--- Project1-Form-Validation/script.js
+++ Project1-Form-Validation/script.js
@@ -4,3 +4,8 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+
+// Create Event Listener for Submit Button
+form.addEventListener("submit", function (e) {
+  console.log("submitted");
+});
