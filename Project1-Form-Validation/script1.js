4b90267cd8f5667c04ed6a26c81e967944298347
Usama Qadeer <66266873+UsamaQadeer05@users.noreply.github.com>
2021-05-12 04:35:33 -0700

    Project 1 JS
    Added Backup script.js code

---
+++ Project1-Form-Validation/script1.js
@@ -0,0 +1,71 @@
+// Retrieving HTML elements from DOM
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Function to Update class and error message
+function showError(input, message) {
+  // Get the parent element of the input field(.form-control)
+  const formControl = input.parentElement;
+  // Override the class - add error
+  formControl.className = "form-control error";
+  // Get the small element for the error message
+  const small = formControl.querySelector("small");
+  // Override the text for small element using the input message
+  small.innerText = message;
+}
+
+// Function to Update class for success
+function showSuccess(input) {
+  // Get the parent element of the input field(.form-control)
+  const formControl = input.parentElement;
+  // Override the class - add success
+  formControl.className = "form-control success";
+}
+
+// Function to Check Email Validate
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Event Listeners
+// Create Event Listener for Submit Button
+form.addEventListener("submit", function (e) {
+  // Stop Page Reloading on Submit Btn
+  e.preventDefault();
+
+  // Check to see if fields meet required field requirements
+  // Check if Username input is empty
+  if (username.value === "") {
+    showError(username, "Username is Required");
+  } else {
+    showSuccess(username);
+  }
+
+  // Check if Email input is empty
+  if (email.value === "") {
+    showError(email, "Email is Required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is Invalid");
+  } else {
+    showSuccess(email);
+  }
+
+  // Check if Password input is empty
+  if (password.value === "") {
+    showError(password, "Password is Required");
+  } else {
+    showSuccess(password);
+  }
+
+  // Check if Password2 input is empty
+  if (password2.value === "") {
+    showError(password2, "Confirm password is Required");
+  } else {
+    showSuccess(password2);
+  }
+});
