81c97209ccbe2c0a02505fd808f5baef606bc4f1
Usama Qadeer <66266873+UsamaQadeer05@users.noreply.github.com>
2021-05-24 23:55:45 +0500

    Start Project 4 script.js
    Committed start of Project 4 scripts in script.js

---
+++ Project4-Exchange-Rate-Calculate/script.js
@@ -0,0 +1,7 @@
+// Get DOM Elements
+const currencyOne = document.getElementById("currency-one");
+const amountcurrencyOne = document.getElementById("amount-one");
+const currencyTwo = document.getElementById("currency-two");
+const amountcurrencyOne = document.getElementById("amount-two");
+const rates = document.getElementById("rate");
+const swap = document.getElementById("swap");
